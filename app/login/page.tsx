d396b4a21d4a3f33fc254c6cc2830f4d807270ce
Duy Vo <vndnetwork@hotmail.com>
2024-04-01 22:54:27 +0700

    Update page titles

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from '@/app/ui/login-form';
 import { Metadata } from 'next';
  
 export const metadata: Metadata = {
-  title: 'Customers',
+  title: 'Login',
 }; 
 
 export default function LoginPage() {
